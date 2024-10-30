2a0e3e423e2dd3930d5d856d631f1113f97d6b79
usuario <usuario@192.168.1.108>
2024-10-30 16:12:55 +0100

    creacion combat

--- src/com/calderon/javamon/Main.java
+++ src/com/calderon/javamon/Main.java
@@ -5,16 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(" Start Program");
 		
-//		Javamon javamon = new Javamon();
-//		javamon.init();
+
 		
 //		Nickname nickname = new Nickname();
 //		String newNickname =nickname.getNewNickname();
 //		System.out.println("El nuevo nickmane es: " + newNickname);
 	
-		Dice dice = new Dice();
-		int diceSide = dice.getRoll();
-		System.out.println("Ha salido " + diceSide);
+//		Dice dice = new Dice();
+//		int diceSide = dice.getRoll();
+//		System.out.println("Ha salido " + diceSide);
+		
+		Javamon javamon1 = new Javamon();
+		System.out.println(javamon1.toString());
+		
+		Javamon javamon2 = new Javamon("Picachu");
+		System.out.println(javamon2.toString());
+		
+		
+		Combat combat = new Combat( javamon1,  javamon2);
+		combat.initCombat();
+		
 		
 		System.out.println("End program");
 
